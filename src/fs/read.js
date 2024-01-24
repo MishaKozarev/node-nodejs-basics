9131e42c5596743317c8adcdcc525fb5411df1dd
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-24 22:45:31 +0300

    feat: implement function fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { ERROR_MESSAGE } from './constants.js';
+
+const URL_FILE_READ = path.resolve('src/fs/files/fileToRead.txt');
 const read = async () => {
-    // Write your code here 
+  try {
+    fs.access(URL_FILE_READ);
+    let fileRead = await fs.readFile(URL_FILE_READ, 'utf-8');
+    console.log(fileRead);
+  } catch {
+    throw new Error(ERROR_MESSAGE);
+  }
 };
 
 await read();
