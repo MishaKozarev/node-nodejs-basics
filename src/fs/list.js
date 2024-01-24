130b29d9a0b5be8e2f6d6ffdc7592bd827c3f6f7
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-24 22:03:41 +0300

    feat: implement function fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,19 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { ERROR_MESSAGE } from './constants.js';
+
+const URL_FILES = path.resolve('src/fs/files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    await fs.access(URL_FILES);
+    let files = await fs.readdir(URL_FILES);
+    console.log(files);
+  } catch(error) {
+      if(error.message !== ERROR_MESSAGE) {
+        throw new Error(ERROR_MESSAGE);
+      } else throw error;
+  }
 };
 
 await list();
