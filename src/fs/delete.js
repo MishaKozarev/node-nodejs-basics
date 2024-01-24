3c5cc5851f9038c98f48bf4969a0dbd1a6300d2e
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-24 21:16:44 +0300

    feat: implement function fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import fs from 'fs/promises';
+import { ERROR_MESSAGE } from './constants.js';
+
+const URL_FILE_DELETE = path.resolve('src/fs/files/fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.unlink(URL_FILE_DELETE);
+  } catch(error) {
+    if(error.message !== ERROR_MESSAGE) {
+      throw new Error(ERROR_MESSAGE);
+    } else throw error;
+  }
 };
 
 await remove();
