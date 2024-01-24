b5ccdadb3b8ea99008955c245f74e232dfb3483a
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-24 18:06:22 +0300

    feat: implement function fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { writeFile } from 'fs/promises';
+import  path from 'path';
+
+const URL = path.resolve('src/fs/files/fresh.txt');
+const TEXT_CONTENT = 'I am fresh and young';
+const ERROR_MESSAGE = 'FS operation failed';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    await writeFile(
+      URL, TEXT_CONTENT, {flag: 'wx'}
+    );
+  } catch {
+    throw Error(ERROR_MESSAGE);
+  }
 };
 
 await create();
