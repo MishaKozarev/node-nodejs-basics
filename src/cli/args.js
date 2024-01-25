a59e5f842f9d79e66889cb6ec9210baf54872fea
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-25 16:32:43 +0300

    feat: implement function cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
-const parseArgs = () => {
-    // Write your code here 
-};
+const PREFIX = '--';
 
+const parseArgs = () => {
+  const args = process.argv;
+  const indexesForPrefix = args.map((_element, index) => {
+    return index;
+  }).filter((index) => args[index].startsWith(PREFIX));
+
+  indexesForPrefix.forEach((index) => {
+    const result = `${args[index].slice(2)} is ${args[index + 1]}`;
+    console.log(result);
+  });
+};
 parseArgs();
