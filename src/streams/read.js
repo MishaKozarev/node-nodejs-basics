f13afeb7c1fab321902f23c3ff38cf6cad820842
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-26 19:41:21 +0300

    feat: implement function streams:read

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,12 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import path from 'path';
+
+const PATH_FILE = path.resolve('src/streams/files/fileToRead.txt');
+
 const read = async () => {
-    // Write your code here 
+  const readStream = createReadStream(PATH_FILE);
+  readStream.pipe(stdout);
 };
 
 await read();
