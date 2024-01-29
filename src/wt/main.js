d4c15515fb5467a63ffa3ee6e7f8b205a52bd331
MishaKozarev <mishaaaaa1986@mail.ru>
2024-01-29 19:36:26 +0300

    feat: implement function wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import { cpus } from 'os';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const performCalculations = async () => {
-    // Write your code here
+  let num = 10;
+
+  const allPromises = cpus().map(() => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(__dirname + '/worker.js', { workerData: num++});
+      worker.on('message', value => resolve(value));
+      worker.on('error', value => reject(value));
+    });
+  })
+
+  const workerResult = await Promise.allSettled(allPromises);
+
+  const result = workerResult.map(({status, value}) => ({
+    status: status === 'rejected' ? 'error' : 'resolved',
+    data: status === 'rejected' ? null : value
+  }));
+
+  console.log(result);
 };
 
 await performCalculations();
+
